42aa19a8ca867fde4bb695112b27fb509229f348
jydoris <yljdoris@foxmail.com>
2019-12-26 09:37:26 +0800

    first commit line drawing

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include "tgaimage.h"
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+
+struct location
+{
+	int m_x;
+	int m_y;
+	location() {}
+	location(int x, int y) { m_x = x; m_y = y; }
+};
+
+void line(location p0, location p1, TGAImage &image, TGAColor color) {
+	
+	if (std::abs(p1.m_y - p0.m_y) > std::abs(p1.m_x - p0.m_x)) {
+		if (p0.m_y > p1.m_y) {
+			std::swap(p0.m_x, p1.m_x);
+			std::swap(p0.m_y, p1.m_y);
+		}
+		for (int y = p0.m_y; y <= p1.m_y; y++) {
+			float t = (float)(y - p0.m_y) / (p1.m_y - p0.m_y);
+			int x = p0.m_x * (1.0 - t) + p1.m_x * t;
+			image.set(x, y, color);
+		}
+	}
+	else {
+		if (p0.m_x > p1.m_x) {
+			std::swap(p0.m_x, p1.m_x);
+			std::swap(p0.m_y, p1.m_y);
+		}
+		for (int x = p0.m_x; x <= p1.m_x; x++) {
+			float t = (float)(x - p0.m_x) / (p1.m_x - p0.m_x);
+			int y = p0.m_y * (1.0 - t) + p1.m_y * t;
+			image.set(x, y, color);
+		}
+	}
+
+	
+}
+
+int main(int argc, char** argv) {
+	TGAImage image(100, 100, TGAImage::RGB);
+	location p0(1, 1);
+	location p1(20, 50);
+	line(p1, p0, image, green);
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga"); 
+		return 0;
+}
