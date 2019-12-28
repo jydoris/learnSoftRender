1df5914c8b219d170efc4ffce0881da1fa3d2f7d
jydoris <yljdoris@foxmail.com>
2019-12-28 11:07:53 +0800

    draw an model from obj files, awsome!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <vector>
 #include "tgaimage.h"
+#include "model.h"
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
+const int width = 800;
+const int height = 800;
 
 struct location
 {
@@ -48,20 +51,25 @@ void line(location p0, location p1, TGAImage &image, TGAColor color) {
 	
 }
 
-std::vector<location> vertex  = {
-	location(1, 1),
-	location(50, 99),
-	location(99, 30),
-	location(1, 1),
-	location(1, 30),
-	location(30, 1)
-};
-
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	
-	for(int i = 0; i < vertex.size(); i+=2)
-		line(vertex[i], vertex[i+1], image, green);
+	TGAImage image(width, height, TGAImage::RGB);
+
+
+	Model *model = new Model("obj/african_head.obj");
+
+
+	for (int i = 0; i<model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j<3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.)*width / 2.;
+			int y0 = (v0.y + 1.)*height / 2.;
+			int x1 = (v1.x + 1.)*width / 2.;
+			int y1 = (v1.y + 1.)*height / 2.;
+			line(location(x0, y0), location(x1, y1), image, white);
+		}
+	}
 
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
